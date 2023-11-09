a4e1ab0333a73699b77192537debc18a6adaec21
xsaven <gms.sw.xsaven@gmail.com>
2023-11-09 17:28:04 +0200

    src/Extension/Config.php

--- src/Extension/Config.php
+++ src/Extension/Config.php
@@ -3,6 +3,7 @@
 namespace Admin\Extend\AdminExceptionReporter\Extension;
 
 use Admin\Core\ConfigExtensionProvider;
+use Admin\ExtendProvider;
 
 /**
  * Class Config
@@ -10,11 +11,14 @@ use Admin\Core\ConfigExtensionProvider;
  */
 class Config extends ConfigExtensionProvider {
 
-    protected $styles = [
-        'vendor/bfg-admin-exception-reporter/prism/prism.css'
-    ];
+    /**
+     * @param  ExtendProvider  $provider
+     */
+    public function __construct(ExtendProvider $provider)
+    {
+        parent::__construct($provider);
 
-    protected $scripts = [
-        'vendor/bfg-admin-exception-reporter/prism/prism.js'
-    ];
+        $this->styles[] = 'vendor/bfg-admin-exception-reporter/prism/prism.css';
+        $this->scripts[] = 'vendor/bfg-admin-exception-reporter/prism/prism.js';
+    }
 }
